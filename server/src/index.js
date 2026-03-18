f99dca7a1080c6c2b74fa88df60d1ad5f3f52709
Khuswant Rajpurohit <khuswant18@gmail.com>
2026-03-18 11:34:51 +0530

    fix(lint): unused variable 'next' in express error handler

--- server/src/index.js
+++ server/src/index.js
@@ -52,6 +52,7 @@ app.use((req, res) => {
   res.status(404).json({ error: 'Route not found' });
 });
 
+// eslint-disable-next-line no-unused-vars
 app.use((err, req, res, next) => {
   console.error('Error:', err);
   res.status(500).json({ error: 'Something went wrong!' });

a48ec0841926fff7d498de32b9cfe52271c49b83
Khuswant Rajpurohit <khuswantrajpurohit18@gmail.com>
2026-05-06 01:30:36 +0530

    fix: change ECS cluster to data source and remove redundant service from terraform

--- infra/main.tf
+++ infra/main.tf
@@ -52,8 +52,8 @@ data "aws_ecr_repository" "ecommerce" {
   name = "ecommerce-backend"
 }
 
-resource "aws_ecs_cluster" "cluster" {
-  name = "ecommerce-cluster"
+data "aws_ecs_cluster" "cluster" {
+  cluster_name = "ecommerce-cluster"
 }
 
 
@@ -84,20 +84,5 @@ resource "aws_ecs_task_definition" "task" {
       ]
     }
   ])
-
-}
-
-resource "aws_ecs_service" "service" {
-  name            = "ecommerce-service"
-  cluster         = aws_ecs_cluster.cluster.id
-  task_definition = aws_ecs_task_definition.task.arn
-  launch_type     = "FARGATE"
-  desired_count   = 1
-
-  network_configuration {
-    subnets          = var.subnet_ids
-    security_groups  = [data.aws_security_group.ecs_sg.id]
-    assign_public_ip = true
-  }
 
 }
